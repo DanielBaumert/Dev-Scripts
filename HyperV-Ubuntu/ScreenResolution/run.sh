c893148a1c22850d18dc580a19be10af8b3e898a
DanielBaumert <daniel.baumert22@gmail.com>
2022-01-27 13:18:45 +0100

    wip hyperv 1920x1080

---
+++ HyperV-Ubuntu/ScreenResolution/run.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+error() {
+  printf '\e[31m'; 
+  printf "$@"; 
+  echo '\e[0m'
+}
+
+is_user_root() { [ "$(id -u)" -eq 0 ]; }
+
+if !(is_user_root); then 
+    error "Please run as root"
+    exit 1
+fi
+
+GRUP_PATH="/ect/default/grup"
+
+sudo cp $GRUP_PATH "${GRUP_PATH}.bak"
+
+file=""
+
+while IFS= read -r line 
+do
+    if $line == "GRUB_CMDLINE_LINUX_DEFAULT=*"; then
+        echo "line"
+    else 
+        file="${file}{$line}"
+    fi
+done < GRUP_PATH
+
+
+echo "${file}"
+
+
