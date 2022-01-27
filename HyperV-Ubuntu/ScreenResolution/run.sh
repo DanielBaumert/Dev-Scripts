d65ab4024c6da20115813f7dfe4aefe2b919ec62
DanielBaumert <daniel.baumert22@gmail.com>
2022-01-27 13:36:40 +0100

    wip

--- HyperV-Ubuntu/ScreenResolution/run.sh
+++ HyperV-Ubuntu/ScreenResolution/run.sh
@@ -21,8 +21,8 @@ file=""
 
 while IFS= read -r line 
 do
-    if $line == "GRUB_CMDLINE_LINUX_DEFAULT=*"; then
-        echo "line"
+    if [[ $line =~ ^GRUB_CMDLINE_LINUX_DEFAULT=\"(.*)\"$ ]] then
+        echo "${BASH_REMATCH[1]}"
     else 
         file="${file}${line}" 
     fi
